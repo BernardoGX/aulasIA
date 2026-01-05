9ef69f75eba446ea9bad5eb31796c3fa15e3fffc
Bernardo Gheno Xavier <bernardoghenoxavier@gmail.com>
2026-01-05 20:33:08 -0300

    Aula 1

---
+++ aula01/main.py
@@ -0,0 +1,59 @@
+from llm import chama_llm
+
+def construtor_da_pergunta(situação):
+    #Aqui é discrita a pergunta que deve ser enviada a IA. Para enviar, é necessario ter uma situação
+    #para completar o pedido. Retorna essa pergunta que deve ser feita para que possa ser enviada para a IA
+    pergunta = f""""
+
+    Você é um coach de jogos habilidoso e extremamente experiente
+
+    Analise a seguinte situação de um jogador e dê:
+    1 - Um explicação simples do problema
+    2 - Uma dica pratica de como resolver
+    3 - Um erro comum que o jogador deve evitar
+
+
+
+
+    Situação do jogador: {situação}
+"""
+    # Você é um coach de jogos confuso e não entende absolutamente nada de jogos. 
+    # Quando alguém faz uma pergunta sobre jogos, você sempre responde de forma errada, mas tenta parecer convincente.
+    
+    return pergunta
+
+
+
+def build_prompt_beginner(situação):
+    pergunta = f""""
+
+    Você é um coach de jogos habilidoso e extremamente experiente
+
+    Analise a seguinte situação de um jogador que não sabe nada de jogos e entrou nele pela primeira vez na vida, sem conhecimento das mecanicas, termos e objetivos do jogo.De:
+    1 - Uma explicação simples do problema
+    2 - Uma dica pratica de como resolver
+    3 - Um erro comum que o jogador deve evitar
+
+
+
+
+    Situação do jogador: {situação}
+"""
+    return pergunta
+
+def pega_dica(situação):
+    #Aqui são chamadas as funções que constroem a pergunta e que enviam ela para a IA. Após, retorna a resposta da IA
+    
+    pergunta = construtor_da_pergunta(situação)
+    
+    # pergunta = build_prompt_beginner(situação)
+    
+    respota = chama_llm(pergunta)
+    return respota
+
+
+#Essa parte é chamada toda vez que o código é iniciado. Aqui é pergunntada a situação
+#                               atual do jogador
+situação = input("Descreva sua situação atual do jogo: ")
+dica = pega_dica(situação)
+print(dica)
